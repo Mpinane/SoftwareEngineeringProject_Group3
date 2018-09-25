06bd00e328a16e6bd648f93e45493742a78062aa
Mpinane <1363679@students.wits.ac.za>
2018-09-25 19:18:37 +0200

    Home template done

--- Labs/Avi System/avi_project/avi_app/urls.py
+++ Labs/Avi System/avi_project/avi_app/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.login, name='login'),
+    path('login/', views.login, name='login'),
     path('create_account/', views.create_account, name='create_account'),
     path('home/', views.home, name='home'),
     path('courses', views.courses, name='courses'),
