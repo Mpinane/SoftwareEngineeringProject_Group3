c0a1fec4632927e44c3981b58e35fee9ed4e6733
Mpinane <1363679@students.wits.ac.za>
2018-09-25 12:30:24 +0200

    deleted manage.py

--- Labs/Avi System/avi_project/avi_app/views.py
+++ Labs/Avi System/avi_project/avi_app/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+from .models import Student
+
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return render(request,'pols/index.html')
