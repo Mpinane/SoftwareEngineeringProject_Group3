9b59aa8da323f535d86780d43b7ba49d89e5eb46
Mpinane <1363679@students.wits.ac.za>
2018-09-27 11:26:51 +0200

    Fixed courses template

--- Labs/Avi System/avi_project/avi_app/static/avi_app/JavaScript/avi.js
+++ Labs/Avi System/avi_project/avi_app/static/avi_app/JavaScript/avi.js
@@ -3,11 +3,7 @@ $(document).ready(function(){
     $('#add_more').click(function(e){
         $('#add_courses').append(
         '<div>'+
-            '<select>'+
-                '{% for i in courses %}'+
-                    '<option name="courses[]" value="{{i}}">{{i}}<option/>'+
-                '{% endfor %}'+
-            '</select>'+
+            '<input name="courses[]" id="courses" placeholder="Enter course code"/>'+
             '<input type="number" placeholder="percentage(%)" name="grades[]"/>'+
             '<input type="button" id="delete_course" value="Delete"/>'+
         '</div>');
@@ -18,4 +14,16 @@ $(document).ready(function(){
     $(this).parent('div').remove();
     }));
 
+    
+
 });
+
+$(function() {
+    var courses = ["coms1000", "coms1008","coms3000"];
+
+    $('#courses').autocomplete({
+        source: courses
+       });
+}
+
+);
--- Labs/Avi System/avi_project/avi_app/views.py
+++ Labs/Avi System/avi_project/avi_app/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+#from django.utils import simplejson
+
 #from .models import Student
 
 
